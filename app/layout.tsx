f7c31c43e3a06845dbbb01a1877bddee7a1d8b71
Zorage <manjeetazim@gmail.com>
2025-01-14 20:18:17 +0530

    Update : Final landing screen

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,13 +24,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        suppressHydrationWarning
       >
         <ThemeProvider
           attribute="class"
-          defaultTheme="dark"
+          defaultTheme="system"
           enableSystem
           disableTransitionOnChange
         >
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
 import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/UI/FloatingNav";
+import { FaHome } from "react-icons/fa";
 
 export default function Home() {
   return (
     <>
       <main className="relative bg-black-100 text-yellow-200 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
         <div className="max-w-7xl w-full">
-        <Hero/>
+          <FloatingNav
+            navItems={[{ name: 'Home', link: '/', icon: <FaHome/> }]}
+          />
+          <Hero />
         </div>
       </main>
     </>
